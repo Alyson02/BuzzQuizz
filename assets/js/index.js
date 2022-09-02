cb50661cab073240a85f723c2b711e59143fabaf
Hugo <nicolau.hugogiles@gmail.com>
2022-09-02 15:38:37 -0300

    Function que ao clicar no quizz tira a tela inicial e vai pra tela do quizz (ainda não indo pro quizz especifico, apenas um generalizado)

--- assets/js/index.js
+++ assets/js/index.js
@@ -20,18 +20,32 @@ async function carregarQuizzes() {
 
   quizzes.forEach((quiz) => {
     section.innerHTML += `
-    <div class="quizz" onclick="carregarQuiz(this) id="${quiz.id}">
+    <div class="quizz" onclick="carregarQuiz(this)" id="${quiz.id}">
         <img src="${quiz.image}" />
         <div class="texto-quizz">${quiz.title}</div>
     </div>
     `;
   });
+  
 }
 
 /**
  * Responsavel por trocar de tela entre todos os quizzes e o quiz em especifico 
  */
 async function carregarQuiz(el){
+
+  let quiz = [];
+  await axios
+    .get(`${urlBase}/quizzes/${el.id}`)
+    .then((r) => (quiz = r.data))
+    .catch((e) => console.log(e));
+  console.log(quiz);
+
+  let tela1 = document.querySelector('.container');
+  let abriuQuizz = document.querySelector('.respostas-quizz');
+  tela1.classList.add('escondido');
+  abriuQuizz.classList.remove('escondido');
+abriuQuizz.scrollIntoView();
 
 }
 
---
+++ assets/js/inicial.js
@@ -0,0 +1,6 @@
+
+function abrirQuizz(numQUizz){
+
+    //ao clicar no Quizz, quero trocar da tela 1 pra tela desse quizz em específico(que é tela 2)
+
+}

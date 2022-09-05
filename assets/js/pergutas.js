64b5a1574f3c4686a4701a975f27213821eb0329
Alyson <80166983+Alyson02@users.noreply.github.com>
2022-09-05 15:04:17 -0300

    fix merge

--- assets/js/pergutas.js
+++ assets/js/pergutas.js
@@ -90,36 +90,35 @@ function montarQuiz(quizzArray) {
   imgCabecalho.src = estruturaQuizz.imagemTitulo;
 
   //Construção do quadro de perguntas e respostas
-  numeroDePerguntas = estruturaQuizz.questoes.length
+  numeroDePerguntas = estruturaQuizz.questoes.length;
   for (let i = 0; i < numeroDePerguntas; i++) {
-    montarQuadro(i)
+    montarQuadro(i);
   }
-
 }
 
 //Quando o usuário selecionar uma resposta está função será chamada
 function selecionarResposta(resposta) {
   resposta.classList.add("item-selecionado"); //adiciona a classe no item selecionado como resposta
 
-  perguntaRespondida++ // Controla quantas perguntas foram respondidas
-  resposta.classList.add('item-selecionado'); //adiciona a classe no item selecionado como resposta
+  perguntaRespondida++; // Controla quantas perguntas foram respondidas
+  resposta.classList.add("item-selecionado"); //adiciona a classe no item selecionado como resposta
 
-  if (resposta.classList.contains('true')) {
-    resposta.classList.add('correto')
-    numeroAcertos++
+  if (resposta.classList.contains("true")) {
+    resposta.classList.add("correto");
+    numeroAcertos++;
   } else {
-    resposta.classList.add('errado')
+    resposta.classList.add("errado");
   }
 
-  let testeResposta = ''
-  const divRespostas = resposta.parentNode //Seleciona toda a div que pertence a resposta
+  let testeResposta = "";
+  const divRespostas = resposta.parentNode; //Seleciona toda a div que pertence a resposta
 
   // console.log('divRespostaNext')
   // console.log(divRespostas.parentNode.nextElementSibling)
 
   // Rolagem para proxima pergunta
   setTimeout(() => {
-    if(divRespostas.parentNode.nextElementSibling){
+    if (divRespostas.parentNode.nextElementSibling) {
       divRespostas.parentNode.nextElementSibling.scrollIntoView({
         behavior: "smooth",
         block: "end",
@@ -128,15 +127,14 @@ function selecionarResposta(resposta) {
     }
   }, 2000);
 
-
   //percorrore por todos os filhos da divResposta
   for (let i = 0; i < alternativas.length; i++) {
-    testeResposta = divRespostas.children[i] //recebe o filho da iteração
+    testeResposta = divRespostas.children[i]; //recebe o filho da iteração
 
-    testeResposta.setAttribute("onclick", " ")
+    testeResposta.setAttribute("onclick", " ");
     //Se o filho não tiver a classe ele item-selecionado ele recebe a classe esbranquicado
-    if (!testeResposta.classList.contains('item-selecionado')) {
-      testeResposta.children[0].classList.add('esbranquicado');
+    if (!testeResposta.classList.contains("item-selecionado")) {
+      testeResposta.children[0].classList.add("esbranquicado");
     }
   }
   if (perguntaRespondida === numeroDePerguntas) {
@@ -146,73 +144,88 @@ function selecionarResposta(resposta) {
 
 // Exibir Resultado do quiz
 function resultadoDoQuizz(numeroAcertos, numeroDePerguntas, level) {
-  let valorPontuacao = Math.round((numeroAcertos / numeroDePerguntas) * 100) //Calcula e aproxima o valor para número interiro
-  let tituloPontuacaco // Titulo associado a pontuação recebido da API
-  let textoPontuacaco // Texto associado a pontuação recebido da API
+  let valorPontuacao = Math.round((numeroAcertos / numeroDePerguntas) * 100); //Calcula e aproxima o valor para número interiro
+  let tituloPontuacaco; // Titulo associado a pontuação recebido da API
+  let textoPontuacaco; // Texto associado a pontuação recebido da API
 
   level.sort(compare); // Organiza em ordem decrescente os nives de pontuação
 
-  for (let i = 0; i < level.length; i++) { // Percorre o objeto da pountuação
-    if (valorPontuacao >= level[i].minValue) { //Se o resultado for >= que o minimo desejado para o nivel 
-      tituloPontuacaco = level[i].title // Recebe o titulo da pontuação
-      imagemPountuacao = level[i].image //Recebe o nivel da pontuação
-      textoPontuacaco = level[i].text // Recebe o texto da pontuação
+  for (let i = 0; i < level.length; i++) {
+    // Percorre o objeto da pountuação
+    if (valorPontuacao >= level[i].minValue) {
+      //Se o resultado for >= que o minimo desejado para o nivel
+      tituloPontuacaco = level[i].title; // Recebe o titulo da pontuação
+      imagemPountuacao = level[i].image; //Recebe o nivel da pontuação
+      textoPontuacaco = level[i].text; // Recebe o texto da pontuação
       break; // Interrompe o for, pois não precisa mais percorrer os níveis inferiores
     }
   }
-  
+
   // Chama a função que irá exibir a tela de resultado
-  // precisa enviar o valorPontuacao, titulo, imagem e texto 
-  exibirResultado(valorPontuacao, tituloPontuacaco, imagemPountuacao, textoPontuacaco)
+  // precisa enviar o valorPontuacao, titulo, imagem e texto
+  exibirResultado(
+    valorPontuacao,
+    tituloPontuacaco,
+    imagemPountuacao,
+    textoPontuacaco
+  );
 }
 
 // Organiza o objeto level em ordem decrescente
 function compare(a, b) {
-  if (a.minValue > b.minValue)
-    return -1;
-  if (a.nome > b.nome)
-    return 1;
+  if (a.minValue > b.minValue) return -1;
+  if (a.nome > b.nome) return 1;
   return 0;
 }
 
-
 //Função que exibe a tela de resultado do Quizz
 //
-function exibirResultado(valorPontuacao, tituloPontuacao, imagemPountuacao, textoPontuacaco){
-
+function exibirResultado(
+  valorPontuacao,
+  tituloPontuacao,
+  imagemPountuacao,
+  textoPontuacaco
+) {
   // Retira a classe escondido para exibir a tela de resultado
-  const divResultado = document.querySelector('.quadro-geral-resultado')
-  divResultado.classList.remove('escondido')
+  const divResultado = document.querySelector(".quadro-geral-resultado");
+  divResultado.classList.remove("escondido");
 
   // Exibe o titulo do resultado
-  const divTituloResultado = document.querySelector('.titulo-resultado')
-  divTituloResultado.innerHTML = `${valorPontuacao}%: ${tituloPontuacao}`
+  const divTituloResultado = document.querySelector(".titulo-resultado");
+  divTituloResultado.innerHTML = `${valorPontuacao}%: ${tituloPontuacao}`;
 
   // Exibe a imagem do resultado
-  const divCorpoResultadoImg = document.querySelector('.corpo-resultado img')
-  divCorpoResultadoImg.src = imagemPountuacao
+  const divCorpoResultadoImg = document.querySelector(".corpo-resultado img");
+  divCorpoResultadoImg.src = imagemPountuacao;
 
   // Exibe o texto do resultado
-  const divDrescicacoResposta = document.querySelector('.descricao-resposta')
-  divDrescicacoResposta.innerHTML =  textoPontuacaco
-  
+  const divDrescicacoResposta = document.querySelector(".descricao-resposta");
+  divDrescicacoResposta.innerHTML = textoPontuacaco;
+
+  const botoes = document.querySelector(".fim-do-quizz");
+  botoes.classList.remove("escondido");
 }
 
 //Função para recarregar o quizz
 
-function recarregarQuizz(){
+function recarregarQuizz() {
   let abriuQuizz = document.querySelector(".questoes");
-  abriuQuizz.innerHTML = '';
- 
+  abriuQuizz.innerHTML = "";
+
   const promessa = axios.get(
     `https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes/${idQuizz}`
   );
   promessa.then(montarQuiz);
   promessa.catch((e) => console.log(e));
 
+  const divResultado = document.querySelector(".quadro-geral-resultado");
+  divResultado.classList.add("escondido");
+
+  const botoes = document.querySelector(".fim-do-quizz");
+  botoes.classList.add("escondido");
 
   //renderizarAlternativas();
-  const topo = document.querySelector('.questoes');
+  const topo = document.querySelector(".questoes");
 
   topo.scrollIntoView(false);
 }
